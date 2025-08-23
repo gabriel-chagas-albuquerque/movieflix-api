8deba37166e82bd06024d7312eadf0f81d46c81f
gabriel-chagas-albuquerque <gabrieldafortaleza0100@gmail.com>
2025-08-23 00:03:08 -0300

    Aplica configurações para o build do projeto

---
+++ dist/src/routes/routes.js
@@ -0,0 +1,166 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+const router = express.Router();
+const prisma = new PrismaClient();
+router.get("/", async (req, res) => {
+    try {
+        const movies = await prisma.movie.findMany({
+            orderBy: {
+                title: "asc",
+            },
+            include: {
+                genres: true,
+                languages: true,
+            },
+            omit: {
+                genre_id: true,
+                language_id: true,
+            },
+        });
+        return res.status(200).json(movies);
+    }
+    catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Houve um erro ao carregar os filmes" });
+    }
+});
+router.get("/:genreName", async (req, res) => {
+    try {
+        const genreName = req.params.genreName;
+        const moviesFilteredByGenre = await prisma.movie.findMany({
+            where: {
+                genres: {
+                    name: {
+                        equals: genreName,
+                        mode: "insensitive",
+                    },
+                },
+            },
+            include: {
+                genres: true,
+                languages: true,
+            }
+        });
+        if (!moviesFilteredByGenre) {
+            return res
+                .status(404)
+                .json({ message: "Não existe filmes com esse gênero" });
+        }
+        return res.status(200).json(moviesFilteredByGenre);
+    }
+    catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Houve um erro na filtragem dos filmes" });
+    }
+});
+router.post("/", async (req, res) => {
+    try {
+        const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+        const verificaDuplicacao = await prisma.movie.findFirst({
+            where: {
+                title: {
+                    equals: title,
+                    mode: "insensitive",
+                },
+            },
+        });
+        if (verificaDuplicacao) {
+            return res
+                .status(409)
+                .json({ message: "Já existe um filme com o mesmo nome" });
+        }
+        const movie = await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+        return res.status(201).json({
+            message: "Filme adicionado com sucesso",
+            data: movie,
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "Houve um problema ao adicionar o filme",
+            erro: err,
+        });
+    }
+});
+router.put("/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+        const verificaExistenciaId = await prisma.movie.findUnique({
+            where: {
+                id,
+            },
+        });
+        if (!verificaExistenciaId) {
+            return res.status(404).json({ message: "O filme não existe" });
+        }
+        const verificaDuplicacao = await prisma.movie.findFirst({
+            where: {
+                title: {
+                    equals: title,
+                    mode: "insensitive",
+                },
+            },
+        });
+        if (verificaDuplicacao) {
+            return res
+                .status(409)
+                .json({ message: "Já existe um filme com o mesmo nome" });
+        }
+        await prisma.movie.update({
+            where: {
+                id,
+            },
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+        return res
+            .status(200)
+            .json({ message: "Informações do filme atualizadas com sucesso" });
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Houve um erro ao atualizar as informações do filme",
+            error: err,
+        });
+    }
+});
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const verificaExistenciaId = await prisma.movie.findUnique({
+            where: {
+                id,
+            },
+        });
+        if (!verificaExistenciaId) {
+            return res.status(404).json({ message: "O filme não existe" });
+        }
+        await prisma.movie.delete({
+            where: {
+                id,
+            },
+        });
+        return res.status(200).json({ message: "Filme deletado com sucesso" });
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Erro ao deletar o filme" });
+    }
+});
+export default router;
+//# sourceMappingURL=routes.js.map
---
+++ dist/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import router from "./routes/routes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" with { type: 'json' };
+const port = 3000;
+const app = express();
+app.use(express.json());
+app.use("/movies", router);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.listen(port, () => {
+    console.log(`Servidor em execução na porta ${port}`);
+});
+//# sourceMappingURL=server.js.map
